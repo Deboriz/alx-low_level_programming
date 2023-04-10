f867c4ccf4df2650d360273e67121f5acda2971b
Deboriz <deborizbenedict@gmail.com>
2023-04-10 14:44:00 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,29 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_binary - A function that prints a binary number without % or /
- * @n: The number to be printed
- * Return: Nothing
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int groin = 0, axe = 32768; /* 1000 0000 0000 0000 */
+	unsigned long int empty;
+	int fits;
 
 	if (n == 0)
 	{
-		_putchar('0');
+		printf("0");
 		return;
 	}
-	while (axe)
+
+	for (empty = n, fits = 0; (empty >>= 1) > 0; fits++)
+		;
+
+	for (; fits >= 0; fits--)
 	{
-		if (groin == 1 && (n & axe) == 0)
-			_putchar('0');
-		else if ((n & axe) != 0)
-		{
-			_putchar('1');
-			groin = 1;
-		}
-		axe >>= 1;
+		if ((n >> fits) & 1)
+			printf("1");
+		else
+			printf("0");
 	}
 }
